cc78dcdc4a33809b2d730a944701f7a9fbb5e249
hasandjidu <144498507+hasandjidu@users.noreply.github.com>
2025-12-02 23:15:38 +0800

    Add files via upload

---
+++ R/run_projectLSA.R
@@ -0,0 +1,13 @@
+#' Launch the projectLSA Shiny Application
+#'
+#' This function starts the Shiny app included in the projectLSA package.
+#'
+#' @return Launches the Shiny application in the default web browser.
+#' @export
+run_projectLSA <- function() {
+  app_dir <- system.file("app", package = "projectLSA")
+  if (app_dir == "") {
+    stop("Could not find Shiny app. Try reinstalling the projectLSA package.", call. = FALSE)
+  }
+  shiny::runApp(app_dir, launch.browser = TRUE)
+}
